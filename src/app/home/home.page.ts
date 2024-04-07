46e3a589a1b1893b8770d759b493ed13e1eaecc8
Jyotirmoy Pal <jyotirmoy.p@eccentricengine.com>
2024-04-07 21:58:33 +0530

    master : calculator WIP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  inputString = ""
+  outputString = ""
+  arithmeticOperators = ['%','/','X','+','-','=']
+  recentPressedValue=""
+  pastValue=""
   constructor() {}
+  onButtonPress(value:any,type?:any){
+    console.log(value,type)
+    // resets the calculator
+    if(type == 'highlight'){
+      if(value == "C"){
+        return
+      }
+      if(this.arithmeticOperators.includes(value)){
+        this.calculateBlock(value)
+        return
+      }
+      if(value=="()"){
+        //
+        return
+      }
+      console.log("HELLO")
+    }else{
+      if(value=="+/-"){
+        //
+        return
+      }
+    }
+  }
+  calculateBlock(data:any){
+    if(data == "=") return this.calculate()
 
+  }
+  reset(){
+    this.inputString = ""
+    this.outputString =""
+    this.recentPressedValue =""
+    this.pastValue =""
+  }
+  calculate(){
+    //
+    return
+  }
 }
